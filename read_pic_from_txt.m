757b45a438bb43f66d72956ef6dff2363b7a08af
Li Xindou <35265150+lixindou2018@users.noreply.github.com>
2018-11-19 11:32:04 +0800

    Update read_pic_from_txt.m

--- read_pic_from_txt.m
+++ read_pic_from_txt.m
@@ -1,16 +1,16 @@
-%从txt文档中读取图片保存；
+%从txt文档中读取图片统一命名并保存；
 clc;
 clear;
 main_path='';
 save_path='';
-fid=fopen([main_path 'train_pos.txt'])
+fid=fopen([main_path 'train_pos.txt']);
 i=0;
-while feof(fid)~=1:
+while feof(fid)~=1
     name=fgetl(fid);
     if isempty(name)
         break;
     end;
-    image_path=[save_path name];
+    image_path=[name]; %[save_path name];
     image=imread(image_path);
     i=i+1;
     img1=imresize(image,[256,256]);
