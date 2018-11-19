28df4ffa449d7e87323e93d23983f887cb89a976
Li Xindou <35265150+lixindou2018@users.noreply.github.com>
2018-11-19 10:30:49 +0800

    Update path2txt.m

--- path2txt.m
+++ path2txt.m
@@ -1,3 +1,7 @@
+% 本段代码的作用，读取图片的路径，并保存在txt文档中。
+% 
+% st包的下载地址：https://pan.baidu.com/s/1atNtiarJ6juuw2B22-VrnA password：cxvh
+%
 clc;
 clear;
 main_path='H:/project/resize/train/';
@@ -5,7 +9,7 @@ save_path='H:/project/resize/train/';
 folder=st.allfolder(main_path)
 
 list_file=[main_path 'trian_pos.txt'];
-fid=fopen(list_file,'a+');
+fid=fopen(list_file,'a+');             %
 for folderIdx=1:length(folder)
     subfolder=st.allfolder([main_path folder{folderIdx}]);
     for subfolderIdx=1:length(subfolder)
