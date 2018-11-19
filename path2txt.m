3fd131579a028593fb962f9006b6b1db4d45f624
Li Xindou <35265150+lixindou2018@users.noreply.github.com>
2018-11-19 10:16:52 +0800

    Update path2txt.m

--- path2txt.m
+++ path2txt.m
@@ -2,7 +2,17 @@ clc;
 clear;
 main_path='H:/project/resize/train/';
 save_path='H:/project/resize/train/';
+folder=st.allfolder(main_path)
 
 list_file=[main_path 'trian_pos.txt'];
-fid=fopen(list_file,'a+')
+fid=fopen(list_file,'a+');
+for folderIdx=1:length(folder)
+    subfolder=st.allfolder([main_path folder{folderIdx}]);
+    for subfolderIdx=1:length(subfolder)
+        files=st.allimage([main_path folder{folderIdx} '/' subfolder{subfolderIdx}])
+            for imageIdx=1:length(files)
+                fprintf(fid,'%s\r\n',[save_path folder{folderIdx} '/' subfolder{subfolderIdx}])
+            end;
+    end;
+end;
 
